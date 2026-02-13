65e22fa410ab1b5dff35c305165a55d33ca8e163
lodwkobku <lodwkobku@gmail.com>
2026-02-13 16:35:08 +0100

    fixed listing heroes in characters

--- wikia/mobile-legends/schemas.ts
+++ wikia/mobile-legends/schemas.ts
@@ -1,9 +1,36 @@
 import { MlbbENDataSource } from "./data-source";
 
 const MlbbEN: ISchema = {
-    url: 'https://mobile-legends.fandom.com/wiki/List_of_heroes',
+    url: 'https://mobile-legends.fandom.com/wiki/Category:Heroes',
+    // pageFormat: 'classic',
     pageFormat: {
-	    selector: `.wikitable tbody tr td:nth-child(2) a`
+        // selector: ".category-page__members a",
+        selector: `a[class="category-page__member-link"]`,
+        ignore: [
+            "Assassin",
+            "Fighter",
+            "Mage",
+            "Marksman",
+            "Support",
+            "Tank",
+            "EXP Laner",
+            "Gold Laner",
+            "Jungler",
+            "Mid Laner",
+            "Roamer",
+            "Side Laner",
+            "Lightborn",
+            "Oriental Fighters",
+            "S.A.B.E.R.",
+            "V.E.N.O.M.",
+            "The Exorcists/Member Introduction",
+            "Popol and Kupa",
+            "Heavenly Artifacts",
+            "Cancelled heroes",
+            "Hero roles",
+            "Hero Specialties",
+            "Category"
+        ]
     },
     dataSource: MlbbENDataSource
 };
